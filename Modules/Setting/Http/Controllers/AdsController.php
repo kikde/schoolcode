7e5c07faa24173437d5eca3f8859b974fd194761
kikde <admin@kikde.com>
2026-01-21 23:20:15 +0530

    fix(ads): rewrite AdsController cleanly and fix style-8 icon; pass  and guard ads table

--- Modules/Setting/Http/Controllers/AdsController.php
+++ Modules/Setting/Http/Controllers/AdsController.php
@@ -3,34 +3,34 @@ namespace Modules\Setting\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Modules\Setting\Entities\Ad;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\\Support\\Facades\\File;
-use Modules\\Setting\\Entities\\Setting;
+use Modules\Setting\Entities\Ad;
+use Modules\Setting\Entities\Setting;
 
 class AdsController extends Controller
 {
-        public function index()
+    public function index()
     {
-        \ = Setting::first();
+        $setting = Setting::first();
+
         if (!Schema::hasTable('ads')) {
             return view('setting::ads.index', [
                 'style7' => null,
                 'style8' => null,
                 'needsMigration' => true,
-                'setting' => \,
+                'setting' => $setting,
             ]);
         }
-        \ = Ad::firstOrCreate(['slot' => 'home_style7']);
-        \ = Ad::firstOrCreate(['slot' => 'home_style8']);
-        return view('setting::ads.index', [
-            'style7' => \,
-            'style8' => \,
-            'setting' => \,
-        ]);
-    }$style7 = Ad::firstOrCreate(['slot' => 'home_style7']);
+
+        $style7 = Ad::firstOrCreate(['slot' => 'home_style7']);
         $style8 = Ad::firstOrCreate(['slot' => 'home_style8']);
-        return view('setting::ads.index', compact('style7','style8'));
+
+        return view('setting::ads.index', [
+            'style7' => $style7,
+            'style8' => $style8,
+            'setting' => $setting,
+        ]);
     }
 
     public function update(Request $request, string $slot)
@@ -42,14 +42,15 @@ class AdsController extends Controller
         if ($request->hasFile('image')) {
             $file = $request->file('image');
             $dest = public_path('backend/uploads/ads');
-            if (!File::exists($dest)) { File::makeDirectory($dest, 0755, true); }
+            if (!File::exists($dest)) {
+                File::makeDirectory($dest, 0755, true);
+            }
             $name = $slot.'_'.time().'.'.$file->getClientOriginalExtension();
             $file->move($dest, $name);
             $data['image_path'] = 'backend/uploads/ads/'.$name;
         }
+
         $ad->fill($data)->save();
         return back()->with('message', ucfirst(str_replace('_',' ', $slot)).' saved');
     }
 }
-
-
