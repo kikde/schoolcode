c627825b0a479a10635e293187003ed8554f205a
kikde <admin@kikde.com>
2026-02-25 19:49:17 +0530

    fix(routes): add named route('about') for /about to satisfy header link

--- routes/web.php
+++ routes/web.php
@@ -100,6 +100,7 @@ Route::post('/send-mail',[FrontendController::class, 'sendmail']);
 Route::get('/contact', [FrontendController::class, 'contactus'])->name('contact');Route::get('/contact/location', [FrontendController::class, 'contactLocation'])->name('contact.location');
 Route::get('/contact/enquiry', [FrontendController::class, 'contactEnquiry'])->name('contact.enquiry');
 
+Route::get('/about', [FrontendController::class, 'aboutUs'])->name('about');
 Route::get('/about/mission-vision', [FrontendController::class, 'aboutMission'])->name('about.mission');
 Route::get('/about/history', [FrontendController::class, 'aboutHistory'])->name('about.history');
 Route::get('/about/leadership', [FrontendController::class, 'aboutLeadership'])->name('about.leadership');
